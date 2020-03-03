ab65dea172a7e27004699fedc50baea15764bc3b
yjl <545436317@qq.com>
2020-03-03 11:20:50 +0800

    version 1.1.0

--- index.js
+++ index.js
@@ -6,20 +6,6 @@ const bodyParser=require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-//解决history模式刷新找不到界面
-var history = require('connect-history-api-fallback')
-//重定向
-app.use(history({
-  rewrites: [
-    { 
-      from: /^\/blogs\/.*$/, 
-      to: function(context) {
-        return context.parsedUrl.path
-      }
-    },
-  ]
-}))
-
 // 路由
 require('./route')(app,express)
 
@@ -27,9 +13,10 @@ require('./route')(app,express)
 const db = require('./connect')
 //用户获取博客
 require('./serverBlogs/getBlogsInfo')(app,db)
-
 //编写博客信息
 require('./serverBlogs/setBlogsInfo')(app,db)
+//网盘
+require('./tools/wangpan')(app,db)
 
 const port = 5000
 app.listen(port,(err) => {
--- route.js
+++ route.js
@@ -1,4 +1,23 @@
+var history = require('connect-history-api-fallback')  //解决history模式刷新找不到界面
 function route(app,express){
+  //重定向
+  app.use(history({
+    rewrites: [
+      { 
+        from: /^\/blogs\/.*$/, 
+        to: function(context) {
+          return context.parsedUrl.path
+        }
+      },
+      {
+        from: /^\/tools\/.*$/, 
+        to: function(context) {
+          return context.parsedUrl.path
+        }
+      },
+    ]
+  }))
+  
   /* 跨域配置 */
   app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -8,18 +27,16 @@ function route(app,express){
     next();  
   });
   
-  //请求界面
   app.use(express.static(__dirname+'/static'))
-  app.use(express.static(__dirname+'/static/dist'))
-  app.get('/', (req, res) => {
+     .use(express.static(__dirname+'/static/dist'))
+     .use(express.static(__dirname+'/static/wangpan'))
+  //请求界面
+  app.get('/', (req,res) => {
     res.sendFile(__dirname + '/static/dist/index.html')
   })
-  
-  //访问图片
-  app.get('/img/:name',(req,res) => {
-    const name = req.params.name
-    const rs = fs.createReadStream(`${__dirname}/static/img/${name}`) //获取图片的文件名
-    rs.pipe(res)
+
+  app.get('/tools/wangpan',(req,res) => {
+    res.sendFile(__dirname + '/static/wangpan/index.html')
   })
 }
 module.exports = route
