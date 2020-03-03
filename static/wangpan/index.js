058bbc058c498bd6ad06aa3c621900d55d13e3bf
yjl <545436317@qq.com>
2020-03-03 11:21:31 +0800

    version 1.1.0

---
+++ static/wangpan/index.js
@@ -0,0 +1,81 @@
+new Vue({
+  el: '#app',
+  data: {
+    files : [],
+    path : ['File'],
+    searchFile : ""
+  },
+  created() {
+    axios.get('http://www.jinlongyuchitang.cn/tools/getWangpanFiles')
+    .then(res => {
+      let data = new Array()
+      res.data.forEach(item => {
+        const keySplit = item.key.split('/')
+        data.push({
+          url : "http://load.jinlongyuchitang.cn/" + item.key + '?attname=',
+          key : item.key,
+          name : keySplit[keySplit.length-1],
+          size : this.renderSize(item.fsize),
+          type : item.mimeType,
+          icon : "icon-picture",
+          keySplit : this.path.concat(keySplit)
+        })
+      })
+      this.files = data
+    })
+  },
+  computed:{
+    currentFiles(){
+      //筛选当前路径
+       let data = this.files.filter(item => {
+         const reg = new RegExp(`^${this.path[this.path.length-1]}`,'gi')
+         return reg.test(item.keySplit[this.path.length-1])
+       })
+      //去重
+       let savePath = []
+       data = data.filter(item => {
+         if(savePath.indexOf(item.keySplit[this.path.length]) === -1){
+           savePath.push(item.keySplit[this.path.length])
+           return true
+         }
+         else if(item.keySplit[this.path.length] === item.name)
+             return true
+         else
+           return false
+       })
+       return data
+    },
+    searchResult(){
+      let data = this.files.filter(item => {
+        const reg = new RegExp(`${this.searchFile}`,'gi')
+        return reg.test(item.name)
+      })
+      console.log(data)
+      return data
+    }
+  },
+  methods:{
+    inDir(name){
+      this.path.push(name)
+    },
+    back(){
+      this.path.splice(-1,1)
+    },
+    navPath(index){
+      this.path.splice(index+1,this.path.length-index-1)
+    },
+    renderSize(value){//格式化文件大小
+      if(null === value || value === '')
+          return "0 Bytes"
+      var unitArr = new Array("Bytes","KB","MB","GB","TB","PB","EB","ZB","YB")
+      var index = 0
+      srcsize = parseFloat(value)
+      index = Math.floor(Math.log(srcsize)/Math.log(1024))
+      var size = srcsize/Math.pow(1024,index)
+      //保留的小数位数
+      size = size.toFixed(2)
+      return size + unitArr[index]
+    }
+  }
+})
+
---
+++ tools/wangpan.js
@@ -0,0 +1,32 @@
+var qiniu = require("qiniu");
+var user = {
+  AK : "oYIp_ibr_Gx0pRf2laF9BYGJVP3kPk0jA3iTXoUe",
+  SK : "sX1JclJwmyx9W-HiHNmxqsE5mdtUopB-CbKktjIG"
+}
+
+var mac = new qiniu.auth.digest.Mac(user.AK, user.SK);
+var config = new qiniu.conf.Config()
+var bucketManager = new qiniu.rs.BucketManager(mac, config)
+
+var bucket = "download121914"
+var options = {
+  limit: 10,
+  prefix: ''
+}
+
+function wangpan(app,db){
+  app.get('/tools/getWangpanFiles',(req,res) => {
+    bucketManager.listPrefix(bucket,options,(err, respBody, respInfo) => {
+      if(err) throw err
+      if(respInfo.statusCode === 200)
+        res.send(respBody.items)
+      else
+        res.send({
+          status : 500,
+          text : "获取失败"
+        })
+    })
+  })
+}
+
+module.exports = wangpan
